0a69e5b4225a530dbf341ad8b528e2a28887484f
Sebastian Venere <sebastian.venere@3sharecorp.com>
2023-05-09 12:43:49 +0200

    missing change of previous commit

--- src/main/java/com/timmydont/wherearetherocas/fetcher/AbstractModelDataFetcher.java
+++ src/main/java/com/timmydont/wherearetherocas/fetcher/AbstractModelDataFetcher.java
@@ -1,5 +1,7 @@
 package com.timmydont.wherearetherocas.fetcher;
 
+import com.timmydont.wherearetherocas.exceptions.ArgumentException;
+import com.timmydont.wherearetherocas.factory.ChartFactory;
 import com.timmydont.wherearetherocas.lib.model.Model;
 import com.timmydont.wherearetherocas.models.enums.DataType;
 import com.timmydont.wherearetherocas.models.enums.Period;
@@ -7,12 +9,13 @@ import com.timmydont.wherearetherocas.services.ModelService;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang3.ObjectUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.log4j.Logger;
 
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static com.timmydont.wherearetherocas.lib.utils.LoggerUtils.error;
 import static com.timmydont.wherearetherocas.utils.DateUtils.toDate;
@@ -21,9 +24,11 @@ public abstract class AbstractModelDataFetcher<T extends Model> implements Model
 
     private final Logger logger = Logger.getLogger(getClass());
 
+    protected final ChartFactory chartFactory;
     protected final ModelService<T> modelService;
 
-    public AbstractModelDataFetcher(ModelService<T> modelService) {
+    public AbstractModelDataFetcher(ModelService<T> modelService, ChartFactory chartFactory) {
+        this.chartFactory = chartFactory;
         this.modelService = modelService;
     }
 
@@ -47,7 +52,6 @@ public abstract class AbstractModelDataFetcher<T extends Model> implements Model
             Date end = getArgument(dataFetchingEnvironment, "end", Date.class);
             Date start = getArgument(dataFetchingEnvironment, "start", Date.class);
             String account = getArgument(dataFetchingEnvironment, "account", String.class);
-            if (!ObjectUtils.allNotNull(start, end, account)) return null;
             // get all items of a given period of time
             List<T> items = modelService.get(account, start, end);
             if (CollectionUtils.isEmpty(items)) {
@@ -62,8 +66,6 @@ public abstract class AbstractModelDataFetcher<T extends Model> implements Model
     public DataFetcher<T> fetchById() {
         return dataFetchingEnvironment -> {
             String id = getArgument(dataFetchingEnvironment, "id", String.class);
-            // check if request argument is valid
-            if (StringUtils.isBlank(id)) return null;
             // get item by id
             T item = modelService.first("id", id);
             if (item == null) {
@@ -83,11 +85,11 @@ public abstract class AbstractModelDataFetcher<T extends Model> implements Model
      * @param <E>         the type of the value
      * @return the value of the argument if exists
      */
-    protected <E extends Object> E getArgument(DataFetchingEnvironment environment, String name, Class<E> clazz) {
+    protected <E extends Object> E getArgument(DataFetchingEnvironment environment, String name, Class<E> clazz) throws ArgumentException {
         Object argument = environment.getArgument(name);
         if (argument == null) {
             error(logger, "request has no argument with name '%s'", name);
-            return null;
+            throw new ArgumentException("request has no argument with name '%s'", name);
         }
         // in case clazz provided is Date, attempt to create a Date from the argument
         if (clazz.equals(Date.class)) return (E) toDate(argument.toString());
@@ -105,15 +107,11 @@ public abstract class AbstractModelDataFetcher<T extends Model> implements Model
      * @param properties  the properties map
      * @return a map of arguments
      */
-    protected Map<String, Object> getArgumentsMap(DataFetchingEnvironment environment, ImmutablePair<String, Class>... properties) {
+    protected Map<String, Object> getArgumentsMap(DataFetchingEnvironment environment, ImmutablePair<String, Class>... properties) throws ArgumentException {
         Map<String, Object> argumentsMap = new HashMap<>();
         for (ImmutablePair<String, Class> property : properties) {
             Object value = getArgument(environment, property.getKey(), property.getValue());
-            if (Objects.isNull(value)) {
-                error(logger, "unable to get an argument of name '%s'", property);
-            } else {
-                argumentsMap.put(property.getKey(), value);
-            }
+            argumentsMap.put(property.getKey(), value);
         }
         return argumentsMap;
     }
